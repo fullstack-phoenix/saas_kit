76bccc6a5a7e59c7ef721acda4c25f836f238daa
Andreas Eriksson <andreas@codered.se>
2022-06-05 14:45:50 +0200

    Update mix file and add correct links

--- mix.exs
+++ mix.exs
@@ -1,19 +1,29 @@
 defmodule SaasKit.MixProject do
   use Mix.Project
 
+  @version "0.2.0"
+
   def project do
     [
       app: :saas_kit,
-      version: "0.2.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      description: "A package for running the code generators from Live SAAS kit.",
+      homepage_url: "https://livesaaskit.com/",
       deps: deps(),
       package: package()
     ]
   end
 
   defp package() do
-    []
+    [
+      licenses: ["MIT"],
+      maintainers: ["Andreas Eriksson"],
+      links: %{
+        "Github" => "https://github.com/fullstack-phoenix/saas_kit"
+      },
+    ]
   end
 
   # Run "mix help compile.app" to learn about applications.
