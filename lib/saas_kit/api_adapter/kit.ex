dbba15551e2c410517a3a6da06433a8da860e58f
Andreas Eriksson <andreas@codered.se>
2023-02-03 07:40:18 +0100

    Add kit-generator and bump version

---
+++ lib/saas_kit/api_adapter/kit.ex
@@ -0,0 +1,48 @@
+defmodule SaasKit.ApiAdapter.Kit do
+  @base_url "https://livesaaskit.com/"
+
+  defp get_api_key, do: Application.get_env(:saas_kit, :api_key)
+  defp get_url, do: (Application.get_env(:saas_kit, :url) || @base_url)
+
+  def get_instructions(token) do
+    token
+    |> get()
+    |> maybe_debug()
+    |> parse_data()
+  end
+
+  defp get(token) do
+    HTTPoison.get("#{get_url()}/api/kits/#{token}", [
+      {"Content-Type", "application/json"},
+      {"x-api-key", get_api_key()}
+    ])
+  end
+
+  defp parse_data({:ok, %HTTPoison.Response{body: body}}) do
+    try do
+      body
+      |> Jason.decode!()
+      |> case do
+        %{"data" => %{"instructions" => instructions}} -> instructions
+        %{"data" => [_|_] = instructions} -> instructions
+        _ -> []
+      end
+    rescue
+      _ ->
+        Mix.shell().error """
+        There was an error parsing the data.
+        """
+        []
+    end
+  end
+
+  defp parse_data(_), do: {:error, "There was an error"}
+
+  defp maybe_debug(data) do
+    if Application.get_env(:saas_kit, :debug) do
+      IO.inspect data
+    end
+
+    data
+  end
+end
