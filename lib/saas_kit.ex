789eaf1dcdf486f34b8a106d407415e736040385
Andreas Eriksson <andreas@codered.se>
2025-10-20 10:34:07 +0200

    Minor fix and bump version

--- lib/saas_kit.ex
+++ lib/saas_kit.ex
@@ -7,7 +7,17 @@ defmodule SaasKit do
     instructions
     |> Enum.filter(fn %{"rule" => rule} -> rule in @allowed_rules end)
     |> Enum.sort_by(fn %{"rule" => rule} -> if rule == "generate_file", do: -1, else: 0 end)
-    |> Enum.each(&follow_instruction(&1, feature))
+    |> Enum.reduce_while([], fn instruction, acc ->
+      if follow_instruction(instruction, feature) == :ok do
+        {:cont, acc}
+      else
+        Mix.shell().info(
+          "#{IO.ANSI.red()}There was an error. Run: mix feature.install #{feature}#{IO.ANSI.reset()}"
+        )
+
+        {:halt, acc}
+      end
+    end)
   end
 
   defp follow_instruction(
@@ -22,6 +32,8 @@ defmodule SaasKit do
       write_file!(filename, template)
       Mix.shell().info("#{IO.ANSI.green()}* Created file:#{IO.ANSI.reset()} #{filename}")
     end
+
+    :ok
   end
 
   defp follow_instruction(
@@ -30,10 +42,10 @@ defmodule SaasKit do
            "last_end" => true,
            "filename" => filename,
            "template" => template
-         } = _instruction,
+         },
          _feature
        ) do
-    if warn_if_file_is_missing(filename) do
+    if warn_if_file_is_missing(filename) && warn_if_file_has_content(filename, template) do
       new_content =
         File.read!(filename)
         |> String.trim_trailing()
@@ -48,6 +60,8 @@ defmodule SaasKit do
 
       Mix.shell().info("#{IO.ANSI.green()}* Updated file:#{IO.ANSI.reset()} #{filename}")
     end
+
+    :ok
   end
 
   defp follow_instruction(
@@ -58,7 +72,7 @@ defmodule SaasKit do
          },
          _feature
        ) do
-    if warn_if_file_is_missing(filename) do
+    if warn_if_file_is_missing(filename) && warn_if_file_has_content(filename, template) do
       existing_content = File.read!(filename)
 
       new_content =
@@ -69,6 +83,8 @@ defmodule SaasKit do
 
       Mix.shell().info("#{IO.ANSI.green()}* Updated file:#{IO.ANSI.reset()} #{filename}")
     end
+
+    :ok
   end
 
   defp follow_instruction(
@@ -79,7 +95,7 @@ defmodule SaasKit do
          },
          _feature
        ) do
-    if warn_if_file_is_missing(filename) do
+    if warn_if_file_is_missing(filename) && warn_if_file_has_content(filename, template) do
       existing_content = File.read!(filename)
 
       new_content =
@@ -90,11 +106,14 @@ defmodule SaasKit do
 
       Mix.shell().info("#{IO.ANSI.green()}* Updated file:#{IO.ANSI.reset()} #{filename}")
     end
+
+    :ok
   end
 
   defp follow_instruction(
          %{
            "rule" => "replace",
+           "smart" => false,
            "filename" => filename,
            "string_to_insert" => "" <> string_to_insert,
            "string_to_replace" => "" <> string_to_replace
@@ -110,17 +129,43 @@ defmodule SaasKit do
 
       Mix.shell().info("#{IO.ANSI.green()}* Updated file:#{IO.ANSI.reset()} #{filename}")
     end
+
+    :ok
+  end
+
+  defp follow_instruction(
+         %{
+           "rule" => "replace",
+           "filename" => filename,
+           "template" => "" <> string_to_insert,
+           "target" => "" <> string_to_replace
+         },
+         _feature
+       ) do
+    if warn_if_file_is_missing(filename) do
+      new_content =
+        File.read!(filename)
+        |> String.replace(string_to_replace, string_to_insert)
+
+      write_file!(filename, new_content)
+
+      Mix.shell().info("#{IO.ANSI.green()}* Updated file:#{IO.ANSI.reset()} #{filename}")
+    end
+
+    :ok
   end
 
   defp follow_instruction(
          %{
            "rule" => rule,
-           "filename" => filename
+           "filename" => filename,
+           "template" => template
          } = instruction,
          feature
        )
-       when rule in ~w(inject_before inject_after) do
+       when rule in ~w(inject_before inject_after replace) do
     with true <- warn_if_file_is_missing(filename),
+         true <- warn_if_file_has_content(filename, template),
          {:ok, content} <- File.read(filename),
          {:ok, updated_content} <-
            get_updated_file(Map.put(instruction, "content", content), feature) do
@@ -132,8 +177,10 @@ defmodule SaasKit do
 
       _ ->
         Mix.shell().error(
-          "#{IO.ANSI.red()}* Failed to install feature:#{IO.ANSI.reset()} #{feature}"
+          "#{IO.ANSI.red()}* Failed to install file:#{IO.ANSI.reset()} #{filename}"
         )
+
+        :error
     end
   end
 
@@ -152,7 +199,7 @@ defmodule SaasKit do
       id: Map.get(instruction, "id")
     }
 
-    case Req.post(url, json: params) do
+    case Req.post(url, json: params, connect_options: [timeout: 60_000]) do
       {:ok, %{body: %{"template" => "" <> template}}} ->
         {:ok, template}
 
@@ -187,6 +234,20 @@ defmodule SaasKit do
       false
     else
       true
+    end
+  end
+
+  defp warn_if_file_has_content(filename, content) do
+    with true <- File.exists?(filename),
+         existing_content <- File.read!(filename),
+         true <- String.contains?("#{existing_content}", "#{content}") do
+      Mix.shell().info(
+        "#{IO.ANSI.yellow()}* file has content, skipping#{IO.ANSI.reset()} #{filename}"
+      )
+
+      false
+    else
+      _ -> true
     end
   end
 end
